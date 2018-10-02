7e2acab4a5c79d84d01053f8dd1f15ea1dcf1ff1
Sebastian <sebastian.landwehr@psi.ch>
2018-10-02 08:32:20 +0200

    added usage of airsea.py tool box (pip install airsea). Added calculation of monin obukov length scale from bulk fluxes

--- pyantarctica/constants.py
+++ pyantarctica/constants.py
@@ -1,26 +1,4 @@
 # list of constants
-
-# physical constants
-
-# van Karman constant used in Boundary layer theory most accepted value = 0.4 (dimensionless)
-vanKarman = 0.4
-
-#Cp: specific heat capacity of (dry) air at constant pressure 
-# at 300K "Tables of Thermal Properties of Gases", NBS Circular 564,1955.
-# https://www.ohio.edu/mechanical/thermo/property_tables/air/air_cp_cv.html
-#Cp = constants.Cp
-Cp = 1005 # J/kg/K
-
-# 
-Levap = 2.5e+6 # J/kg
-
-rho_air = 1 # kg/m3
-
-# gravity [m/s^2]
-g = 9.81
-#def Gravity():
-    # could define gravity based on latitude
-#    g=9.81 # const of gravitation
-#    return g
-    #An alternative formula for g as a function of latitude is the WGS (World Geodetic System) 84 Ellipsoidal Gravity Formula:[17]
-    #g { ϕ } = G e [ 1 + k sin 2 ⁡ ϕ 1 − e 2 sin 2 ⁡ ϕ ] , {\displaystyle g\{\phi \}=\mathbb {G} _{e}\left[{\frac {1+k\sin ^{2}\phi }{\sqrt {1-e^{2}\sin ^{2}\phi }}}\right],\,\!} g\{\phi\}= \mathbb{G}_e\left[\frac{1+k\sin^2\phi}{\sqrt{1-e^2\sin^2\phi}}\right],\,\!
+# decided to use the airsea.py toolbox of Filipe Fernandes 
+# leave this hear in case we need to add other constants
+# e.g.: from airsea import contants as constants
